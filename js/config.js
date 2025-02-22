ff99eb84226f3bbe3e2e5d29e2adae64cb8522de
Abrahm <aouedra2@jh.edu>
2025-02-22 11:06:58 -0800

    updating config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_yAfKsS33a', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6g8prit6j8s5nnv6gqtvqva1r2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
